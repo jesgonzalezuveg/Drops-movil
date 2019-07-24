3647dc7fb7a107caf0201f601e669dd4e108a81d
jetovarr <47257581+jetovarr@users.noreply.github.com>
2019-07-24 16:06:55 -0500

    dbManager alterar tablas

--- Drops mobil/Assets/Development/Jesus/Scripts/dataBaseManager.cs
+++ Drops mobil/Assets/Development/Jesus/Scripts/dataBaseManager.cs
@@ -67,33 +67,52 @@ public class dataBaseManager : MonoBehaviour
         Tablas tablasJson = JsonUtility.FromJson<Tablas>(jsonString);
         for (int i = 0; i < ordenTablas.Length; i++) {
             foreach (var tabJson in tablasJson.tabla) {
-                if (tabJson.nombre == ordenTablas[i]) {
-                    string res = consultarTablas(tabJson.nombre);
-                    if (res == "0") {
-                        Debug.Log("No encontro la tabla " + tabJson.nombre);
-                        Debug.Log("Crear tabla en la bd");
-                    } else {
-                        tableFound = true;
-                        ColumnasDB columnasDB = JsonUtility.FromJson<ColumnasDB>("{\"colums\" : [" + res + "]}");
-                        foreach (var colDB in columnasDB.colums) {
-                            foreach (var colJson in tabJson.colum) {
-                                if (colDB.name == colJson.nombre) {
-                                    columFound = true;
-                                    Debug.Log(colDB.name);
-                                } else {
-                                    Debug.Log("El nombre de la columna actual no coincide.");
-                                }
-                            }
-                            if (columFound == false) {
-                                Debug.Log("Borrar columna de la bd");
-                            }
-                            columFound = false;
-
-                        }
-                    }
-                } else {
-                    Debug.Log("El nombre de la tabla actual no coincide.");
+                foreach (var col in tabJson.colum) {
+                    crearColumna(col, tabJson.nombre);
+                    return;
                 }
+                //if (tabJson.nombre == ordenTablas[i]) {
+                //    string res = consultarTablas(tabJson.nombre);
+                //    if (res == "0") {
+                //        Debug.Log("No encontro la tabla " + tabJson.nombre);
+                //        Debug.Log("Crear tabla en la bd");
+                //    } else {
+                //        tableFound = true;
+                //        ColumnasDB columnasDB = JsonUtility.FromJson<ColumnasDB>("{\"colums\" : [" + res + "]}");
+
+                //        //Elimar columanas que no esten en el json
+                //        foreach (var colDB in columnasDB.colums) {
+                //            foreach (var colJson in tabJson.colum) {
+                //                if (colDB.name == colJson.nombre) {
+                //                    columFound = true;
+                //                } else {
+                //                    Debug.Log("El nombre de la columna de la db actual no coincide.");
+                //                }
+                //            }
+                //            if (columFound == false) {
+                //                Debug.Log("Borrar columna de la bd");
+                //            }
+                //            columFound = false;
+                //        }
+
+                //        //Agregar y verificar estructura de tablas que contenga el json
+                //        foreach (var colJson in tabJson.colum) {
+                //            foreach (var colDB in columnasDB.colums) {
+                //                if (colDB.name == colJson.nombre) {
+                //                    columFound = true;
+                //                } else {
+                //                    Debug.Log("El nombre de la columna actual del json no coincide.");
+                //                }
+                //            }
+                //            if (columFound == false) {
+                //                Debug.Log("Agregar columna a la bd");
+                //            }
+                //            columFound = false;
+                //        }
+                //    }
+                //} else {
+                //    Debug.Log("El nombre de la tabla actual no coincide.");
+                //}
             }
         }
     }
@@ -104,9 +123,91 @@ public class dataBaseManager : MonoBehaviour
         
     }
 
+   void queryCrearTabla(Columnas tabla) {
+        //string query = "CREATE TABLE IF NOT EXISTS \"aviso2\" (\"id\"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \"fechaVisualizacion\"    datetime NOT NULL, \"idPaquete\" int(11) NOT NULL, \"idUsuario\" int(11) NOT NULL, \"descripcion\"   VARCHAR(50), FOREIGN KEY(\"idUsuario\") REFERENCES \"usuario\"(\"id\"),FOREIGN KEY(\"idPaquete\") REFERENCES \"paquete\"(\"id\")); ";
+        string query;
+        string fkStr = "";
+        bool fk = false;
+        int count = 0;
+        query = "BEGIN TRANSACTION;  CREATE TABLE IF NOT EXISTS \"" + tabla.nombre+"2\" (";
+        foreach (var col in tabla.colum) {
+            count++;
+            query += "\""+col.nombre+"\"";
+            if (col.nombre == "id") {
+                query += " INTEGER";
+            } else {
+                if (col.size != "") {
+                    query += " " + col.tipo + "(" + col.size + ")";
+                } else {
+                    query += " " + col.tipo + "";
+                }
+            }
+
+            if (col.NN == "1") {
+                query += " NOT NULL";
+            }
+
+            if (col.PK == "1") {
+                query += " PRIMARY KEY";
+            }
+
+            if (col.AI == "1") {
+                query += " AUTOINCREMENT";
+            }
+
+            if (col.fkTable != "") {
+                fk = true;
+                fkStr += " FOREIGN KEY(\""+col.nombre+"\") REFERENCES \""+col.fkTable+"\"(\"id\"),"; 
+            }
+
+            query += ",";
+        }
+
+        if (fk != true) {
+            query = query.Substring(0, query.Length - 1);
+        }
+        else{
+            fkStr = fkStr.Substring(0, fkStr.Length - 1);
+            query += fkStr;
+        }
+
+        query += ");  COMMIT;";
+
+
+        Debug.Log(query);
+        ejecutarQuery(query, "Se creo la tabla correctamente");
+    }
+
+    void crearColumna(Data dataColum, string nombreTabla) {
+        string query;
+        query = "BEGIN TRANSACTION; "; 
+        if (dataColum.size !="") {
+            query += "ALTER TABLE " + nombreTabla + " ADD " + dataColum.nombre + " " + dataColum.tipo+";";
+        } else {
+            query += "ALTER TABLE " + nombreTabla + " ADD " + dataColum.nombre + " " + dataColum.tipo+"("+dataColum.size+");";
+        }
+        query += " COMMIT;";
+        ejecutarQuery(query, "Se creo la columna correctamente");
+
+    }
+
+    void cambiarTipoColumna(Data dataColum, string nombreTabla) {
+        string query;
+        query = "ALTER TABLE " + nombreTabla + " ALTER COLUMN " + dataColum.nombre + " " + dataColum.tipo + ";";
+        ejecutarQuery(query, "Se creo la columna correctamente");
+
+    }
+
     public static string consultarTablas(string nombreTabla) {
         string query = "PRAGMA table_info("+nombreTabla+");";
         var result = conexionDB.selectGeneral(query);
         return result;
+    }
+
+    public static void ejecutarQuery(string queryEjecutar, string descripcion) {
+        string query = queryEjecutar;
+        var result = conexionDB.selectGeneral(query);
+        Debug.Log(result);
+        Debug.Log(descripcion);
     }
 }
