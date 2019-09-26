753f0efe28fc7e51ab6cb761312bda6d6c9984b8
Abaraj <andreslinklol@gmail.com>
2019-09-26 10:57:52 -0500

    Animaciones Nuevas Personaje
    Se añadieron nuevas animaciones a la mascota, click en cabeza y body

---
+++ Drops mobil/Assets/Development/Andy/Scripts/touchChara.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchChara : MonoBehaviour
+{
+    public Animator animController;
+    
+    void Start(){
+        animController = GetComponent<Animator>();
+        
+    }
+
+    IEnumerator volverIdle()
+    {
+        yield return new WaitForSeconds(.2f);
+        animController.SetBool("toHead", false);
+        animController.SetBool("toScreen", false);
+    }
+
+
+    public void toHead()
+    {
+        if (animController){
+            animController.SetBool("toHead", true);
+            StartCoroutine(volverIdle());
+        }
+       
+    }
+
+
+    public void toScreen()
+    {
+        if (animController){
+            animController.SetBool("toScreen", true);
+            StartCoroutine(volverIdle());
+        }
+    }
+
+}
