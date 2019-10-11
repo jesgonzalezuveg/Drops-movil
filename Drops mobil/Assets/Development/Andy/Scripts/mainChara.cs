cd125290518f77da33b89e5e9b042b2807eb8b60
jesgonzalezuveg <47258265+jesgonzalezuveg@users.noreply.github.com>
2019-10-11 12:20:20 -0500

    Anim Fix

--- Drops mobil/Assets/Development/Andy/Scripts/mainChara.cs
+++ Drops mobil/Assets/Development/Andy/Scripts/mainChara.cs
@@ -2,65 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainChara : MonoBehaviour
-{
+public class mainChara : MonoBehaviour{
     Animator mainAnim;
 
     bool touchHead;
     bool toScreen;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         mainAnim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch (0);
-			Vector2 touchPos = Camera.main.ScreenToWorldPoint (touch.position);
-
-            Debug.Log(touchPos);
-
-			// processing touch phases
-			switch (touch.phase) {
-
-			// when you touch the screen for the first time and if you touches a body collider then animation is allowed
-			// so if you touch another screen point then animation will not start
-			// in another words you have to touch a body to start an animation
-            //Function of toHead
-			case TouchPhase.Began:
-				if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (touchPos)) {
-					touchHead = true;
-				}
-				break;
-
-				// if you move your finger touching a body then animation is playing by setting animators isBodyStroked variable to true
-			case TouchPhase.Moved:
-				if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (touchPos) && touchHead) {
-					mainAnim.SetBool ("toHead", true);
-				}
-
-				// if your finger is off the body then animation stops by setting animators isBodyStroked variable to false
-				if (GetComponent<Collider2D> () != Physics2D.OverlapPoint (touchPos)) {
-					mainAnim.SetBool ("toHead", false);
-				}
-				break;
-
-				// if you stop moving your finger then animation stops by setting animators isBodyStroked variable to false
-			case TouchPhase.Stationary:
-				mainAnim.SetBool ("toHead", false);
-				break;
-
-				// if you release your finger then animation stops by setting animators isBodyStroked variable to true
-				// and animation is not allowed anymore until you touch a body again
-			case TouchPhase.Ended:
-				mainAnim.SetBool ("toHead", false);
-				touchHead = false;
-				break;
-			}
-		}
+    public void touchHeadFunc() {
+        mainAnim.SetBool("toScreen", true);
+        StartCoroutine(endAnim("toScreen"));
     }
+
+    public void touchBodyFunc() {
+        mainAnim.SetBool("toHead", true);
+        StartCoroutine(endAnim("toHead"));
+    }
+
+    IEnumerator endAnim(string nombre) {
+        yield return new WaitForSeconds(1f);
+        mainAnim.SetBool(nombre, false);
+    }
+
 }
